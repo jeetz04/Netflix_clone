c141cf538a1b6b5c1cbd922869775709e3ff22ad
jeet <jeetchoudharyz04@gmail.com>
2024-04-04 15:13:24 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,7 @@
+// var dropdownToggle = document.getElementById('dropdown-toggle');
+// var dropdownContent = document.getElementById('dropdown-content');
+
+// // Toggle the 'show' class on click
+// dropdownToggle.addEventListener('click', function() {
+//   dropdownContent.classList.toggle('show');
+// });
